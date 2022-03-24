b6c99f8babbb82bc7a1dd9efc04fa005e511dae0
Elijah Oluwaseyifunmi Banjo <elijahbanjo@gmail.com>
2022-03-24 17:59:30 +0100

    Update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const app = express();
 const dotenv = require('dotenv');
 
-mongoose.connect('mongodb+srv://seyiadmin:seyiadmin@cluster0.dkvlt.mongodb.net/UserAPI?authSource=admin&replicaSet=atlas-xnqpr7-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', ()=>{
+mongoose.connect('mongodb:localhost/userapi', ()=>{
     console.log('Database Connected'), e => {console.log(`Could not connect because of ${e.message}`)}
 });
 
